cb578e48a6674366908ae511ea685376916f9645
DAVI ROBERTO MACHADO VILA <davi.vila@embraer.com.br>
2024-09-03 15:03:36 -0300

    Dashboard Azure DIO

---
+++ README.md
@@ -0,0 +1,19 @@
+# Desafio2_Projeto_PowerBi_Dio
+Projeto Final
+
+1 - Criado conta na Azure.
+
+2 - Criado um banco de dados no Azure para poder iniciar o projeto.
+
+3 - Aplicado ajustes de rede e permissões devidas para acesso e edição no Power BI Desktop.
+
+4 - Baixado certificado e validado.
+
+5 - Efetuado a conexão com workbench para edição do script e inserção de informações e valores conforme projeto da DIO.
+
+6 - Após edição conforme, aberto no PowerBI, dados transofrmados no PowerQuery para edição do relatório.
+
+7 - No Power Query, você pode combinar duas consultas de duas maneiras: Acrescentar Consultas e Mesclar Consultas. A principal diferença entre as duas é que Acrescentar Consultas adiciona linhas à tabela, enquanto Mesclar Consultas adiciona colunas à tabela.
+A opção Acrescentar Consultas é útil quando você deseja combinar as duas tabelas em uma única tabela adicionando dados. Por outro lado, a opção Mesclar Consultas é útil quando você deseja combinar informações de duas tabelas em uma única.
+
+8 - Efetuado um relatorio simples de página unica em desktop e mobile.
---
+++ azure_company.sql
@@ -0,0 +1,232 @@
+use azure_company;
+select * from information_schema.table_constraints
+	where constraint_schema = 'azure_company';
+    CREATE TABLE employee(
+	Fname varchar(15) not null,
+    Minit char,
+    Lname varchar(15) not null,
+    Ssn char(9) not null, 
+    Bdate date,
+    Address varchar(30),
+    Sex char,
+    Salary decimal(10,2),
+    Super_ssn char(9),
+    Dno int not null,
+    constraint chk_salary_employee check (Salary> 2000.0),
+    constraint pk_employee primary key (Ssn)
+);
+
+alter table employee 
+	add constraint fk_employee 
+	foreign key(Super_ssn) references employee(Ssn)
+    on delete set null
+    on update cascade;
+    alter table employee modify Dno int not null default 1;
+    
+   desc employee; 
+   
+   create table departament(
+	Dname varchar(15) not null,
+    Dnumber int not null,
+    Mgr_ssn char(9) not null,
+    Mgr_start_date date, 
+    Dept_create_date date,
+    constraint chk_date_dept check (Dept_create_date < Mgr_start_date),
+    constraint pk_dept primary key (Dnumber),
+    constraint unique_name_dept unique(Dname),
+    foreign key (Mgr_ssn) references employee(Ssn)
+);
+
+   alter table departament drop departament_ibfk_1;
+   alter table departament 
+		add constraint fk_dept foreign key(Mgr_ssn) references employee(Ssn)
+        on update cascade;
+        
+        desc departament;
+        
+	create table dept_locations(
+	Dnumber int not null,
+	Dlocation varchar(15) not null,
+    constraint pk_dept_locations primary key (Dnumber, Dlocation),
+    constraint fk_dept_locations foreign key (Dnumber) references departament (Dnumber)
+);
+  
+alter table dept_locations
+drop foreign key fk_dept_locations;
+
+alter table dept_locations 
+	add constraint fk_dept_locations foreign key (Dnumber) references departament(Dnumber)
+	on delete cascade
+    on update cascade;
+    
+    create table project(
+	Pname varchar(15) not null,
+	Pnumber int not null,
+    Plocation varchar(15),
+    Dnum int not null,
+    primary key (Pnumber),
+    constraint unique_project unique (Pname),
+    constraint fk_project foreign key (Dnum) references departament(Dnumber)
+);
+
+create table works_on(
+	Essn char(9) not null,
+    Pno int not null,
+    Hours decimal(3,1) not null,
+    primary key (Essn, Pno),
+    constraint fk_employee_works_on foreign key (Essn) references employee(Ssn),
+    constraint fk_project_works_on foreign key (Pno) references project(Pnumber)
+);
+
+drop table dependent;
+create table dependent(
+	Essn char(9) not null,
+    Dependent_name varchar(15) not null,
+    Sex char,
+    Bdate date,
+    Relationship varchar(8),
+    primary key (Essn, Dependent_name),
+    constraint fk_dependent foreign key (Essn) references employee(Ssn)
+);
+
+show tables;
+
+desc dependent;
+
+select * from employee;
+
+use azure_company;
+
+alter table employee drop constraint fk_employee;
+
+alter table employee 
+	add constraint fk_employee 
+	foreign key(Super_ssn) references employee(Ssn)
+    on delete set null
+    on update cascade;
+    
+    alter table employee modify Dno int not null default 1;
+    
+update employee
+set Super_ssn = '333445555'
+where Ssn = '888665555';
+
+
+select * from employee;
+
+INSERT INTO employee (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES 
+('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
+('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
+('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
+('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
+('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
+('Joyce', 'A', 'English', 453453453, '1972-07-31', '5631-Rice-Houston-TX', 'F', 25000, 333445555, 5),
+('Ahmad', 'V', 'Jabbar', 987987987, '1969-03-29', '980-Dallas-Houston-TX', 'M', 25000, 987654321, 4),
+('James','E','Borg','888665555','1937-11-10','450-Stone-Houston-TX','M','55000','01010101','1');
+
+insert into dependent values (333445555, 'Alice', 'F', '1986-04-05', 'Daughter'),
+							 (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
+                             (333445555, 'Joy', 'F', '1958-05-03', 'Spouse'),
+                             (987654321, 'Abner', 'M', '1942-02-28', 'Spouse'),
+                             (123456789, 'Michael', 'M', '1988-01-04', 'Son'),
+                             (123456789, 'Alice', 'F', '1988-12-30', 'Daughter'),
+                             (123456789, 'Elizabeth', 'F', '1967-05-05', 'Spouse');
+                             
+insert into departament values ('Research', 5, 333445555, '1988-05-22','1986-05-22'),
+							   ('Administration', 4, 987654321, '1995-01-01','1994-01-01'),
+                               ('Headquarters', 1, 888665555,'1981-06-19','1980-06-19');
+                               
+insert into dept_locations values (1, 'Houston'),
+								 (4, 'Stafford'),
+                                 (5, 'Bellaire'),
+                                 (5, 'Sugarland'),
+                                 (5, 'Houston');
+                                 
+insert into project values ('ProductX', 1, 'Bellaire', 5),
+						   ('ProductY', 2, 'Sugarland', 5),
+						   ('ProductZ', 3, 'Houston', 5),
+                           ('Computerization', 10, 'Stafford', 4),
+                           ('Reorganization', 20, 'Houston', 1),
+                           ('Newbenefits', 30, 'Stafford', 4);
+                           
+insert into works_on values (123456789, 1, 32.5),
+							(123456789, 2, 7.5),
+                            (666884444, 3, 40.0),
+                            (453453453, 1, 20.0),
+                            (453453453, 2, 20.0),
+                            (333445555, 2, 10.0),
+                            (333445555, 3, 10.0),
+                            (333445555, 10, 10.0),
+                            (333445555, 20, 10.0),
+                            (999887777, 30, 30.0),
+                            (999887777, 10, 10.0),
+                            (987987987, 10, 35.0),
+                            (987987987, 30, 5.0),
+                            (987654321, 30, 20.0),
+                            (987654321, 20, 15.0),
+                            (888665555, 20, 0.0);
+                            
+select * from employee;
+SELECT e.Ssn, COUNT(d.Essn) 
+FROM employee e, dependent d 
+WHERE (e.Ssn = d.Essn) 
+GROUP BY e.Ssn;
+
+select * from dependent;
+
+SELECT Bdate, Address FROM employee
+WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
+
+select * from departament where Dname = 'Research';
+
+SELECT Fname, Lname, Address
+FROM employee, departament
+WHERE Dname = 'Research' AND Dnumber = Dno;
+
+select * from project;
+
+select Dname as Department, Mgr_ssn as Manager from departament d, dept_locations l
+where d.Dnumber = l.Dnumber;
+
+select Dname as Department, concat(Fname, ' ', Lname) from departament d, dept_locations l, employee e
+where d.Dnumber = l.Dnumber and Mgr_ssn = e.Ssn;
+
+select * from project, departament where Dnum = Dnumber and Plocation = 'Stafford';
+
+SELECT Pnumber, Dnum, Lname, Address, Bdate
+FROM project, departament, employee
+WHERE Dnum = Dnumber AND Mgr_ssn = Ssn AND
+Plocation = 'Stafford';
+
+
+SELECT * FROM employee WHERE Dno IN (3,6,9);
+
+SELECT Bdate, Address
+FROM EMPLOYEE
+WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
+
+SELECT Fname, Lname, Address
+FROM EMPLOYEE, DEPARTAMENT
+WHERE Dname = 'Research' AND Dnumber = Dno;
+
+select Fname, Lname, Salary, Salary*0.011 from employee;
+
+select Fname, Lname, Salary, Salary*0.011 as INSS from employee;
+select Fname, Lname, Salary, round(Salary*0.011,2) as INSS from employee;
+
+-- definir um aumento de salário para os gerentes que trabalham no projeto associado ao ProdutoX
+select e.Fname, e.Lname, 1.1*e.Salary as increased_sal from employee as e,
+works_on as w, project as p where e.Ssn = w.Essn and w.Pno = p.Pnumber and p.Pname='ProductX';
+
+-- concatenando e fornecendo alias
+select Dname as Department, concat(Fname, ' ', Lname) as Manager from departament d, dept_locations l, employee e
+where d.Dnumber = l.Dnumber and Mgr_ssn = e.Ssn;
+
+-- recuperando dados dos empregados que trabalham para o departamento de pesquisa
+select Fname, Lname, Address from employee, departament
+	where Dname = 'Research' and Dnumber = Dno;
+
+-- definindo alias para legibilidade da consulta
+select e.Fname, e.Lname, e.Address from employee e, departament d
+	where d.Dname = 'Research' and d.Dnumber = e.Dno;
+
